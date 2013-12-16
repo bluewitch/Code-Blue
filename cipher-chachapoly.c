05f9f68988790e4c7adcf2e3e7d03c4b427b576a
Douglas Kuhn <bluewitchproductions@gmail.com>
2013-12-16 12:29:16 -0800

    Update cipher-chachapoly.c

--- cipher-chachapoly.c
+++ cipher-chachapoly.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2013 Damien Miller <djm@mindrot.org>
- *
+ *  ~dwulf
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
