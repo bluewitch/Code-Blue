24a6489a97d9cd4de54f1ee7d56bcc305f44e878
Douglas Kuhn <bluewitchproductions@gmail.com>
2014-01-15 17:45:38 -0800

    Update clock_angle.js

--- clock_angle.js
+++ clock_angle.js
@@ -4,10 +4,10 @@ Be as precise as you can.
 */
 
 // h = 1..12, m = 0..59
-static double angle(int h, int m) {
-    double hAngle = 0.5D * (h * 60 + m);
-    double mAngle = 6 * m;
-    double angle = Math.abs(hAngle - mAngle);
-    angle = Math.min(angle, 360 - angle);
-    return angle;
+function angle(h,m) {
+hAngle = 0.5 * (h * 60 + m);
+mAngle = 6 * m;
+angle = Math.abs(hAngle - mAngle);
+angle = Math.min(angle, 360 - angle);
+return angle;
 }
