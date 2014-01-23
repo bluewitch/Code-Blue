aafa6d7b9183c3fde310cdf770f12229968121e5
Douglas Kuhn <bluewitchproductions@gmail.com>
2014-01-22 20:39:41 -0800

    Update clock_angle.js

--- clock_angle.js
+++ clock_angle.js
@@ -1,9 +1,13 @@
 /*
 Given a 12-hour analog clock, compute in degree the smaller angle between the hour and minute hands. 
 Be as precise as you can.
+~dwulf
 */
 
 // h = 1..12, m = 0..59
+var h = 6;
+var m = 30;
+
 function angle(h,m) {
 hAngle = 0.5 * (h * 60 + m);
 mAngle = 6 * m;
