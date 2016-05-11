2725ff8366611022f6a1917875a7b3179a1d11e5
Douglas Kuhn <bluewitchproductions@gmail.com>
2016-05-11 08:42:32 -0700

    Update clock_angle.js

--- clock_angle.js
+++ clock_angle.js
@@ -1,17 +1,47 @@
 /*
-Given a 12-hour analog clock, compute in degree the smaller angle between the hour and minute hands. 
-Be as precise as you can.
+angle.js
 ~dwulf
+For the last fucking time
+	We create a function named angle with two parameters
+	named hour and minute.
+
+	We then create a variable and instantiate it with the 
+	new and improved let keyword, ECMA 2016 compliant.
+
+	We create hAngle as a variable, and calculate from
+	data in our hour and minute parameters using 
+	appropriate operators.
+
+	We then create mAngle as a variable, and calculate 
+	from the data of the minute parameter using 
+	appropriate operators.
+
+	We create angle as a variable, and calculate from
+	data in hAngle and mAngle variables using JavaScript
+	native Math.abs object to calculate a absolute number.
+	Subtracting value in hAngle from value in mAngle
+
+	We then take the value from that calculation and
+	calculate again using JavaScript native Math.min object
+	to return the smallest of zero or more numbers. 
+	Subtracting 360 from the value in the variable angle.
+
+	We then return the new value in the variable angle.
+
+	THAT is your ANSWER to determine the angles of a 
+	clock based on the reletive position of the clock 
+	hands!
+
 */
+"use strict";
 
-// h = 1..12, m = 0..59
-var h = 6;
-var m = 30;
+function angle( hour, minute ) 
+{
+	let hAngle = 0.5 * ( hour * 60 + minute );
+	let mAngle = 6 * minute;
 
-function angle(h,m) {
-hAngle = 0.5 * (h * 60 + m);
-mAngle = 6 * m;
-angle = Math.abs(hAngle - mAngle);
-angle = Math.min(angle, 360 - angle);
+	let angle = Math.abs( hAngle - mAngle );
+	    angle = Math.min( angle, 360 - angle );
+
 return angle;
 }
